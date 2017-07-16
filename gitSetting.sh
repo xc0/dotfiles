adc9c312da7fe7dcac0f42994ac52ab4f3fe6e0c
xc0 <xcd0@xcd0.com>
2017-07-17 02:06:35 +0900

    init.shがlinuxの部分のみとりあえず完成

--- gitSetting.sh
+++ gitSetting.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+count=0
 while true; do
 	echo -n "gitで使用するユーザー名を入力してください : "
 	read name
@@ -11,11 +12,19 @@ while true; do
 		elif [ "${check}" == "n" ]; then
 			break
 		else
+			((count++))
 			echo "yかnを入力します"
+			if [ "${count}" == "3" ]; then
+				echo "3回の誤入力があったため異常終了します"
+				exit 1
+			else
+				echo "誤入力$count回目です 3回で終了します"
+			fi
 			continue
 		fi
 	done
 	if [ "${check}" == "y" ]; then
+		count=0
 		break
 	else
 		continue
@@ -33,11 +42,19 @@ while true; do
 		elif [ "${check}" == "n" ]; then
 			break
 		else
+			((count++))
 			echo "yかnを入力します"
+			if [ "${count}" == "3" ]; then
+				echo "3回の誤入力があったため異常終了します"
+				exit 1
+			else
+				echo "誤入力$count回目です 3回で終了します"
+			fi
 			continue
 		fi
 	done
 	if [ "${check}" == "y" ]; then
+		count=0
 		break
 	else
 		continue
@@ -55,17 +72,26 @@ while true; do
 		elif [ "${check}" == "n" ]; then
 			break
 		else
+			((count++))
 			echo "yかnを入力します"
+			if [ "${count}" == "3" ]; then
+				echo "3回の誤入力があったため異常終了します"
+				exit 1
+			else
+				echo "誤入力$count回目です 3回で終了します"
+			fi
 			continue
 		fi
 	done
 	if [ "${check}" == "y" ]; then
+		count=0
 		break
 	else
 		continue
 	fi
 done
 
+exit 0
 
 git config --global user.name "$name"
 git config --global user.email "$mail"
@@ -77,3 +103,6 @@ echo "alias gpl='git pull origin master'" >> ~/.bashrc
 echo "alias gps='git push origin master'" >> ~/.bashrc
 
 echo "gitの設定が完了しました"
+
+exit 0
+
--- init.sh
+++ init.sh
@@ -3,71 +3,106 @@
 # クリーンインストールしたら
 # とりあえず最初に実行しとけ
 
+echo -n "goの設定までスキップしますか？ (Y/n) : "
+read check
+start=`date +%s`
+
+if [ "${check}" == "n" ]; then
+
+# gitSetting {{{
+count=0
 while true; do
-	echo -n "このファイルinit.shは~/work/dotfilesに置かれていますか？ (y/n) : "
+	echo -n "gitの初期設定をしますか？ (y/N) : "
 	read check
 	if [ "${check}" == "y" ]; then
+		echo "gitの設定をします"
+		~/work/dotfiles/gitSetting.sh
+		if [ "${?}" == "0" ]; then
+			echo "gitの設定が正常終了しました"
+			echo "アプリケーションのインストールを始めます"
+			break
+		else
+			echo "gitの設定が正しく終了しませんでした"
+			continue
+		fi
+	else
+		echo "gitの設定をスキップします"
 		break
-	elif [ "${check}" == "n" ]; then
+	fi
+done
+
+# }}}
+
+# よく使うアプリケーションのインストール {{{
+count=0
+
+while true; do
+	echo -n "このファイルinit.shは~/work/dotfilesに置かれていますか？ (Y/n) : "
+	read check
+	if [ "${check}" == "n" ]; then
 		echo "init.shは~/work/dotfiles/init.shに置かれなければなりません"
 		echo "正しい位置に置いてください"
 		echo "mkdir ~/work"
 		echo "cd ~/work"
 		echo "git clone http://git:kumamon@git.xcd0.com/git/dot.git"
 		echo "でできます"
+		echo "異常終了します"
 		exit 1
+	else
+		break
 	fi
-	echo "yかnです"
 done
 
+count=0
 
 
 if [ "$(uname)" == 'Darwin' ]; then
 	OS='Mac'
-	while true; do
-		echo -n "Macですね? (y/n) : "
-		read check
-		if [ "${check}" == "y" ]; then
-			break
-		else
-			echo "まじっすか... OSが判断できませんでした"
-			echo "終了します"
-			exit 1
-		fi
-	done
-
+	echo -n "Macですね? (Y/n) : "
+	read check
+	if [ "${check}" == "n" ]; then
+		echo "まじっすか... OSが判断できませんでした"
+		echo "手作業で実行してください"
+		echo "異常終了します"
+		exit 1
+	else
+		echo "未実装"
+		break
+	fi
 elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
 	OS='Linux'
-	while true; do
-		echo "Debian系のみ実行してね♥ (RHEL? 知らない子ですね)"
-		echo -n "Linuxですね? (y/n) : "
-		read check
-		if [ "${check}" == "y" ]; then
-			break
-		else
-			echo "まじっすか... OSが判断できませんでした"
-			echo "終了します"
-			exit 1
-		fi
-	done
+	echo "Debian系のみ実行してね♥ (RHEL? 知らない子ですね)"
+	echo -n "Linuxですね? (Y/n) : "
+	read check
+	if [ "${check}" == "n" ]; then
+		echo "まじっすか... OSが判断できませんでした"
+		echo "手作業で実行してください"
+		echo "異常終了します"
+		exit 1
+	fi
 
 	# 英語化
+	echo -e "\n=======================\n"
 	echo "ホームディレクトリを英語化します"
-	echo "export LANG=C xdg-user-dirs-gtk-update"
-	export LANG=C xdg-user-dirs-gtk-update
-	export LANG=ja_JP.UTF-8
+	echo "export LANG=C; xdg-user-dirs-gtk-update"
+	bash `export LANG=C; xdg-user-dirs-gtk-update` &
 
+	echo -e "\n=======================\n"
 	echo "ソースリストをアップデートします"
 	echo "sudo aptitude update"
-	sudo aptitude update
+	echo "不要なときはC-cでスキップします"
+	sudo aptitude update > /dev/null 2>&1
+	echo -e "\n=======================\n"
 	echo "いろいろインストールします"
 	echo "sudo aptitude install -y make cmake gcc vim curl wget"
 	sudo aptitude install -y make cmake gcc vim curl wget
 	echo "sudo aptitude install -y python-dev python-pip python3-dev python3-pip"
+	sudo aptitude install -y python-dev python-pip python3-dev python3-pip
 	echo "pip3 install neovim"
 	pip3 install neovim
 
 	# latex
+	echo -e "\n=======================\n"
 	echo "Latex関係のインストールをします"
 	echo "sudo aptitude install -y tex-live-lang-cjk"
 	sudo aptitude install -y tex-live-lang-cjk
@@ -75,28 +110,30 @@ elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
 	sudo mktexlsr
 
 	# neovimのインストール
-	sudo aptitude install software-properties-common
+	sudo aptitude install -y software-properties-common
+	echo -e "\n=======================\n"
 	echo "リポジトリを追加しようとします"
 	echo "ダメでも気にしない"
 	sudo add-apt-repository ppa:neovim-ppa/unstable
-	sudo aptitude update
+	echo "sudo aptitude update > /dev/null 2>&1"
+	echo "不要なときはC-cでスキップします"
+	sudo aptitude update > /dev/null 2>&1
 	echo "neovimをインストールします"
-	sudo aptitude install neovim
+	sudo aptitude install -y neovim
 
 elif [ "$(expr substr $(uname -s) 1 10)" == 'MINGW32_NT' ]; then
 	OS='Cygwin'
-	while true; do
-		echo -n "windowsですね? (y/n) : "
-		read check
-		if [ "${check}" == "y" ]; then
-			break
-		else
-			echo "まじっすか... OSが判断できませんでした"
-			echo "終了します"
-			exit 1
-		fi
-	done
-
+	echo -n "windowsですね? (Y/n) : "
+	read check
+	if [ "${check}" == "n" ]; then
+		echo "まじっすか... OSが判断できませんでした"
+		echo "手作業で実行してください"
+		echo "異常終了します"
+		exit 1
+	else
+		echo "未実装"
+		break
+	fi
 else
 	echo "Your platform ($(uname -a)) is not supported."
 	exit 1
@@ -109,56 +146,145 @@ fi
 # OS共通
 
 # pyenv
+echo -e "\n=======================\n"
 echo "pyenvをインストールします"
 curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
 
 # rbenv
+echo -e "\n=======================\n"
 echo "rbenvをインストールします"
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 
-# golang
+# }}}
 
-echo "golangのコンパイラをダウンロードします"
-git clone https://github.com/golang/go
+fi
 
-dir=$HOME/work/go
-cd $dir
+# golang {{{
 
-cp -r go go1.4
-cd go1.4
+echo -e "\n=======================\n"
+echo "goの最新版をビルドします"
+
+mkdir ~/work/go/
+cd ~/work/go
+
+if [ -d ~/work/go/go ]; then
+	echo "~/work/go/goが存在します"
+	cd ~/work/go/go
+	echo -n "最新版に更新しますか? (y/N) : "
+	read check
+	if [ "${check}" == "y" ]; then
+		echo "git pull origin master"
+		git pull origin master
+		echo "git reset --hard HEAD"
+		git reset --hard HEAD
+	fi
+else
+	echo "goの最新版をダウンロードします"
+	echo "git clone https://github.com/golang/go"
+	git clone https://github.com/golang/go
+fi
+
+
+cp -rf ~/work/go/go ~/work/go/go1.4
+
+godir=$HOME/work/go
+GOROOT=$HOME/work/go
+cd $godir/go1.4
 
 # go1.4
-
-echo "go1.4をビルドします"
-git checkout release-branch.go1.4	>/dev/null 2>&1
-export GOROOT_BOOTSTRAP=$dir/go1.4
-if [ "$(uname)" == 'Darwin' ]; then
-	$dir/go1.4/src/make.bash
-elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
-	$dir/go1.4/src/make.bash
-elif [ "$(expr substr $(uname -s) 1 10)" == 'MINGW32_NT' ]; then
-	$dir/go1.4/src/make.bat
+if [ -d ~/work/go/go1.4/bin ]; then
+	echo "go 1.4が存在します"
+else
+	echo -e "\n=======================\n"
+	echo "go1.4をビルドします"
+	echo "git checkout release-branch.go1.4	>/dev/null 2>&1"
+	git checkout release-branch.go1.4	>/dev/null 2>&1
+	cd $godir/go1.4/src
+	if [ "$(uname)" == 'Darwin' ]; then
+		./make.bash
+	elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+		./make.bash
+	elif [ "$(expr substr $(uname -s) 1 10)" == 'MINGW32_NT' ]; then
+		./make.bat
+	fi
+	echo "go1.4のビルドが完了しました"
 fi
-export PATH=$dir/go1.4/bin:$PATH
+PATH="$godir/go1.4/bin:$PATH"
+echo $PATH
+which go
+go version
 
-echo "最新版のgolangをビルドします"
-cd $dir/go/src
-if [ "$(uname)" == 'Darwin' ]; then
-	$dir/go/src/all.bash
-elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
-	$dir/go/src/all.bash
-elif [ "$(expr substr $(uname -s) 1 10)" == 'MINGW32_NT' ]; then
-	$dir/go/src/all.bat
+
+if [ -d ~/work/go/go/bin ]; then
+	flag=1
+	echo "~/work/go/goが存在します"
+	echo -n "ビルドし直しますか? (y/N) : "
+	read check
+	if [ "${check}" == "y" ]; then
+		flag=0
+	fi
+else
+	flag=0
 fi
+
+if [ "${flag}" == "0" ]; then
+
+	echo -e "\n=======================\n"
+	echo "最新版のgolangをビルドします"
+
+	rm -rf $HOME/go1.4
+	ln -sf $godir/go1.4 $HOME
+
+	cd $godir/go/src
+
+	if [ "$(uname)" == 'Darwin' ]; then
+		./all.bash
+	elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+		./all.bash
+	elif [ "$(expr substr $(uname -s) 1 10)" == 'MINGW32_NT' ]; then
+		./all.bat
+	fi
+
+	rm -rf $HOME/go1.4
+
+	echo -e "\n=======================\n"
+	echo "最新版のgolangのビルドが完了しました"
+fi
+
+
+PATH=$godir/go/bin:$PATH
+which go
+go version
 
 
 cd ~
-mkdir work/usr
-mkdir work/bin
-mkdir work/usr/local
-mkdir work/usr/local/bin
+if [ -d ~/work/usr ]; then
+	echo -n ""
+else
+	mkdir ~/work/usr
+fi
+if [ -d ~/work/bin ]; then
+	echo -n ""
+else
+	mkdir ~/work/bin
+fi
+if [ -d ~/work/usr/local ]; then
+	echo -n ""
+else
+	mkdir ~/work/usr/local
+fi
+if [ -d ~/work/usr/local/bin ]; then
+	echo -n ""
+else
+	mkdir ~/work/usr/local/bin
+fi
 
+# }}}
+
+# dotfiles {{{
+
+echo -e "\n=======================\n"
 echo "dotfileのシンボリックリンクを作成します "
 if [ "$(uname)" == 'Darwin' ]; then
 	OS='Mac'
@@ -174,11 +300,79 @@ else
 	exit 1
 fi
 
-for f in `ls $dir`; do
-	ln -s $dir/$f
-done
+date=`date +%s`
+echo "リンク先ファイル一覧"
+ls -al $dir
+echo ""
 
-echo "gitの設定をします"
-~/work/dotfiles/gitSetting.sh
+if [ -L ~/.bashrc ]; then
+	rm ~/.bashrc
+fi
+if [ -f ~/.bashrc ]; then
+	echo ".bashrcが存在します"
+	mv ~/.bashrc ~/.vimrc_$date
+	echo ".bashrc_$dateにリネームしました"
+fi
 
-echo "すべての処理が完了しました"
+if [ -L ~/.vimrc ]; then
+	rm ~/.vimrc
+fi
+if [ -f ~/.vimrc ]; then
+	echo ".vimrcが存在します"
+	mv ~/.vimrc ~/.vimrc_$date
+	echo ".vimrc_$dateにリネームしました"
+fi
+
+if [ -L ~/.config/vim ]; then
+	rm ~/.config/vim
+fi
+if [ -d ~/.config/vim ]; then
+	echo "~/.config/vimが存在します"
+	mv ~/.config/vim ~/.config/vim_$date
+	echo "~/.config/vim_$dateにリネームしました"
+fi
+
+if [ -d ~/.config/nvim ]; then
+	rm ~/.config/nvim
+fi
+if [ -d ~/.config/nvim ]; then
+	echo ".config/nvimが存在します"
+	mv ~/.config/nvim ~/.config/nvim_$date
+	echo "~/.config/nvim_$dateにリネームしました"
+fi
+
+ln -sf $dir/.bashrc
+ln -sf $dir/.vimrc
+
+cd ~/.config/
+ln -sf $dir/.config/nvim
+ln -sf $dir/.config/vim
+
+
+echo -e "\n=======================\n"
+echo "シンボリックリンクを作成しました"
+
+
+echo ""
+echo "$HOME"
+ls -al ~ | grep .bashrc
+ls -al ~ | grep .vimrc
+echo ""
+echo "~/.config/vim"
+ls -al ~/.config/vim
+echo ""
+echo "~/.config/nvim"
+ls -al ~/.config/nvim
+
+# }}}
+
+echo -e "\n=======================\n"
+echo "以上ですべての処理が完了しました"
+echo -e "\n=======================\n"
+end=`date +%s`
+past=$((end - start))
+echo "実行時間 : $past"
+
+exit 0
+
+
